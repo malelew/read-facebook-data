1478cc8d1137260781bd47f317d71d859c041a14
malelew@ucsd.edu <SoloMune@Mos-MacBook-Pro.local>
2016-12-31 17:57:50 -0600

    updated README to reflect current state of project and modified read-data.py to work on more general uses

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ucbmeft-data
-A look at the Berkley meme FB group membership
+This started out as a look at the Berkley meme FB group membership after the
+moderators decided to limit further membership to Cal students and alumni.
+The project ran into complications with FB api access permissions but the
+read-data.py file is still useful for gathering Facebook data.
+
+The read-data.py execution takes in a CSV with that contains a row of 
+Facebook IDs writes to separate CSV the dict object related to the ID.
+For any GraphAPI errors the row in the CSV will contain a single F.
+
+Example Execution: "python read-data.py fb-data.csv fb-output.csv"
+
+Before exeucting make sure to include a facebook api access token as detailed
+here, https://developers.facebook.com/docs/facebook-login/access-tokens. Also
+the FB_ID_COLUMN variable corressponds with the column containing the FB ids.
+
+If you would rather look into other data and relations than you can adjust the
+get_object with anything present in the documentation:
+http://facebook-sdk.readthedocs.io/en/latest/api.html
--- read-data.py
+++ read-data.py
@@ -5,18 +5,36 @@ import lxml
 import requests
 
 #Facebook token
-FB_ACCESS_TOKEN=''
+FB_ACCESS_TOKEN='ADD YOUR ACCESS TOKEN HERE'
 
 #setup facebook graph api
 graph = facebook.GraphAPI(access_token=FB_ACCESS_TOKEN)
 
+#constant for the column containting the profile IDs
+FB_ID_COLUMN = 2
+
 #open and read the csv fie
 with open(sys.argv[1], 'rb') as f:
+    #create csv read object
     reader = csv.reader(f)
     i = 0
-    for row in reader:
-        if i != 0:
-            obj = graph.get_object(row[0])
-            print obj
-        i+=1
 
+    #open and write results to new csv
+    with open(sys.argv[2], 'w') as output:
+        #create csv write object
+        ret_val = csv.writer (output)
+
+        #for loop thru the facebook ID and get profile object
+        for row in reader:
+            try:
+                #skips header
+                if i != 0:
+                    #change this line if you're interested in other data or relations
+                    obj = graph.get_object(row[FB_ID_COLUMN])
+                    #writes the dict object's items returned from get call
+                    ret_val.writerow(obj.items())
+            #issues with profile permission caught to check if isolated issue
+            except facebook.GraphAPIError:
+                #write F to indicate failed access
+                ret_val.writerow('F')
+            i+=1
